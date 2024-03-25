a6f4a14182511c0165cb41fd03bc7b63278b699f
arthurpelitoo <arthur.pelito@escola.pr.gov.br>
2024-03-25 16:07:12 -0300

    alteracoes no sharebutton, threedot, texttitle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import ShareButton from './Components/ShareButton';
+import ThreeDotIcon from './Components/ThreeDotIcon';
 
 function App() {
+
+  const testebacanudo = () =>{ 
+      alert('testebacanudo');
+  }
+
+
   return (
     <>
       <div id='Container'>
-        
+        <ShareButton onClick={testebacanudo} />
       </div>
     </>
   )
