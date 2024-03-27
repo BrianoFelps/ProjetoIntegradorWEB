8b405c9f392c5a494bf1f3ca07b3442f1eb1fe8a
arthurpelitoo <arthur.pelito@escola.pr.gov.br>
2024-03-27 14:10:10 -0300

    ultimas alteracoes topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import './App.css'
-import TopBar from './all-in-one/Topbar'
+
 
 function App() {
   return (
     <>
-      <div id='Container'>
-        <TopBar></TopBar>
-      </div>
     </>
   )
 }
