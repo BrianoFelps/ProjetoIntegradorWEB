242d00f198a8f695aa0bc50b0eb00ba0a7a645e7
arthurpelitoo <arthur.pelito@escola.pr.gov.br>
2024-05-29 14:34:41 -0300

    menores correçoes, porem quero descobrir o que fazer

--- src/Components/LastCreated.tsx
+++ src/Components/LastCreated.tsx
@@ -1,20 +1,20 @@
 import './LastCreated.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock } from '@fortawesome/free-regular-svg-icons';
-import {format} from 'date-fns'
-import { ptBR } from 'date-fns/locale'
+// import {format} from 'date-fns'
+// import { ptBR } from 'date-fns/locale'
 
 function LastCreated (){
-    const formattedDate = format(new Date(), "d 'de' MMMM 'de' yyyy HH:mm", { locale: ptBR });
+    // const formattedDate = format(new Date(), "d 'de' MMMM 'de' yyyy HH:mm", { locale: ptBR });
 
     return(
         <div id='ContainerCreated' className='d-flex justify-content-between'>
             <div className='btn btn-light BotaoComponent'>
                 <FontAwesomeIcon icon={faClock}/> Criado
             </div>
-            <div className='conteudo'>
+            {/* <div className='conteudo'>
                 {formattedDate}
-            </div>
+            </div> */}
         </div>
     )
 }
