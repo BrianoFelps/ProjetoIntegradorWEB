0dc2a321ef01e6f99933790a30e47224ebb92f36
Brian <142152802+BrianoFelps@users.noreply.github.com>
2024-06-03 15:52:52 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,20 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MasterPage from './Pages/MasterPage';
-
-<!-- import Login from './Pages/Login';
-import SignUp from './Pages/SignUp'; -->
+import Login from './Pages/Login';
+import SignUp from './Pages/SignUp';
 
 
 function App() {
   return (
     <>
-<!--         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/homepage" element={<MasterPage/>} />
-            <Route path="/SignUp" element={<SignUp/>} />
-          </Routes>
-        </BrowserRouter> -->
-      <MasterPage/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/homepage" element={<MasterPage/>} />
+          <Route path="/SignUp" element={<SignUp/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
