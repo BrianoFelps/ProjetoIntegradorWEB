e10b1198d7d53718ee857068c1e457cdd868f5f1
BrianoFelps <briansticknodesbr@gmail.com>
2025-07-30 16:40:20 -0300

    Correção 2

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           <Route path="/" element={<Login setUser={setUserName} />} />
           <Route path="/homepage" element={<MasterPage userName={userName} />} />
           <Route path="/SignUp" element={<SignUp setUser={setUserName}/>} />
-          <Route path="/User" element={<UserInfo userName={userName}/>} />
+          <Route path="/UserInfo" element={<UserInfo userName={userName}/>} />
         </Routes>
       </BrowserRouter>
     </>
